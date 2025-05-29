00b40a77a89a869eb30d9a60842985c32549cbad
anatolii <batkovich327@gmail.com>
2025-05-29 17:32:27 +0200

    Done Task 1 - 3 Data Types 2

--- python_start/data_types.py
+++ python_start/data_types.py
@@ -1,4 +1,8 @@
 ##### Math and data types
+print('\n[=============================================================]\n')
+print('[Data Types 1]')
+print('\n[=============================================================]\n')
+
 print('\tMath opeerations: ')
 
 input('Type \'Start\'==> ')
@@ -48,7 +52,6 @@ print("\nResult: ", round(cost_of_the_trip, 2), "currency units")
 
 """
 
-
 ###### Task 2: Simple vehicle telemetry
 """
 Description:
@@ -94,15 +97,119 @@ print('\n3] Parking lot ')
 hours = int(input("Count of hours? "))
 time_of_day = input("Day or night (D/N): ").strip().upper()
 
-first_2_hours_rate = 20
-after_2_hours_rate = 15
+first_two_hours_rate = 20
+after_two_hours_rate = 15
 
 if hours <= 2:
-    cost = hours * first_2_hours_rate
+    cost = hours * first_two_hours_rate
 else:
-    cost = 2 * first_2_hours_rate + (hours - 2) * after_2_hours_rate
+    cost = 2 * first_two_hours_rate + (hours - 2) * after_two_hours_rate
 
 if time_of_day == 'N':
     cost *= 0.5
 
 print("Result: ", round(cost, 2), "UAH")
+
+print('\n[=============================================================]\n')
+print('[Data Types 2]')
+print('\n[=============================================================]\n')
+
+"""
+Задача 1: Конфігуратор автомобіля
+
+Опис:
+Запроси у користувача такі дані:
+
+    Марка авто (str)
+    Рік випуску (int)
+    Ціна (float)
+    Чи електро? (bool через так/ні)
+
+Виведи:
+
+    Тип кожної змінної
+    Короткий опис:
+    Ваш автомобіль Tesla 2020 року коштує 45600.0 €. Електро: True.
+"""
+
+auto_logo = input('Марка вашого авто |==> ')
+year_present = int(input('Рік випуску |==> '))
+price = float(input('Ціна |==> '))
+is_electric = bool(input('Електроавтомобіль ? |==> '))
+
+print(type(auto_logo), type(year_present), type(price), type(is_electric))
+print('Ваш автомобіль {} {} року, коштує {}. Електро: {}'.format(auto_logo, year_present, price, is_electric))
+
+"""
+Задача 2: Конвертер кілометрів у милі та інші одиниці
+
+Опис:
+Користувач вводить відстань у км. Програма виводить:
+
+    В милях (1 км = 0.621371 миль)
+
+    У метрах
+
+    У сантиметрах
+
+    Вивести тип кожного результату.
+"""
+
+print('\nkm converter\n')
+num = float(input('Type number km |==> '))
+ONE_MILE_IN_KM = 0.621371
+
+print('Mile: ', num * ONE_MILE_IN_KM)
+print('SM: ', num * 100000)
+print('M: ', num * 1000)
+
+"""
+Задача 3 (комплексна): Автомобільний аукціон
+
+Опис:
+Є 3 автомобілі з такими характеристиками:
+
+car1_name = "BMW"
+car1_year = 2018
+car1_price = 22000.50
+
+car2_name = "Audi"
+car2_year = 2020
+car2_price = 27000.00
+
+car3_name = "Tesla"
+car3_year = 2021
+car3_price = 32000.99
+
+Завдання:
+
+    Обчисли середню ціну авто
+
+    Знайди найновіше авто
+
+    Створи змінну description з рядком:
+    Tesla (2021) – 32000.99 €
+"""
+
+car1_name = "BMW"
+car1_year = 2018
+car1_price = 22000.50
+
+car2_name = "Audi"
+car2_year = 2020
+car2_price = 27000.00
+
+car3_name = "Tesla"
+car3_year = 2021
+car3_price = 32000.99
+
+avg_price = (car1_price + car2_price + car3_price) / 3
+
+print('AVG price: {}'.format(avg_price))
+
+if (car1_year > car2_year & car1_year > car3_year):
+    print( '{} ({}) - {} €'.format(car1_name, car1_year, car1_price))
+elif (car2_year > car1_year & car2_year > car3_year):
+    print( '{} ({}) - {} €'.format(car2_name, car2_year, car2_price))
+else:
+    print( '{} ({}) - {} €'.format(car3_name, car3_year, car3_price))
